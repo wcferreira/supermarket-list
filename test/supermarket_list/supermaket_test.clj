96258bca1f50f26d1838bec964ea47594c8a16c7
Wagner Ferreira <wcarvalho.ferreira@gmail.com>
2020-09-22 10:51:54 -0300

    test: remove item by name

--- test/supermarket_list/supermaket_test.clj
+++ test/supermarket_list/supermaket_test.clj
@@ -9,7 +9,7 @@
   (println "teardown")
   (sm/remove-all!))
 
-(use-fixtures :once
+(use-fixtures :each
               (fn [tests]
                 (setup)
                 (tests)
@@ -18,8 +18,18 @@
 (deftest test-can-add-item
   (let [item {:name "Rice" :quantity 1 :price 8.99M}]
     (sm/add-item! item)
-    ;(println (deref sm/supermarket))
+    (println (deref sm/supermarket))
     (is (= (get (filterv (fn [v] (= (:name item) (:name v))) (deref sm/supermarket)) 0) item))))
+
+
+(deftest test-can-remove-item-by-name
+  (sm/add-item! {:name "Sugar" :quantity 2 :price 4.67M})
+  (sm/add-item! {:name "Juice" :quantity 1 :price 10.55M})
+  (sm/add-item! {:name "Beans" :quantity 3 :price 8.33M})
+  (println (deref sm/supermarket))
+  (sm/remove-item-by-name! "Juice")
+  (println (deref sm/supermarket))
+  (is (true? (empty? (filter (fn [item] (= (:name item) "Juice")) (deref sm/supermarket))))))
 
 
 
