7e6f874d53ca5550c01cb19156a9c3515e3974df
Wagner Ferreira <wcarvalho.ferreira@gmail.com>
2020-09-22 09:17:07 -0300

    test: add item

--- test/supermarket_list/supermaket_test.clj
+++ test/supermarket_list/supermaket_test.clj
@@ -1,2 +1,30 @@
 (ns supermarket-list.supermaket-test
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [supermarket-list.supermarket :as sm]))
+
+(defn setup []
+  (println "setup"))
+
+(defn teardown []
+  (println "teardown"))
+
+(use-fixtures :once
+              (fn [tests]
+                (setup)
+                (tests)
+                (teardown)))
+
+(deftest test-can-add-item
+  (let [item {:name "Rice" :quantity 1 :price 8.99M}]
+    ;(sm/add-item! item)
+    ;(println (deref sm/supermarket))
+    (is (= (get (filterv (fn [v] (= (:name item) (:name v))) (deref sm/supermarket)) 0) item))))
+
+
+
+
+
+
+
+
+
